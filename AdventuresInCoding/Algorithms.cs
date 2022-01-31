f7f1b11a6bc292f19f911a754fa042cab11e7377
Brian Seabolt <brianmseabolt@gmail.com>
2022-01-31 09:55:32 -0500

    adding more randomness

---
+++ AdventuresInCoding/Algorithms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIC.Functionality
+{
+    public static class Algorithms
+    {
+        public static int GetFactorial(int n) => throw new NotImplementedException();
+
+        public static string BuildFormattedString(List<string> items) => throw new NotImplementedException();
+    }
+}
---
+++ AdventuresInCoding/Tests/AlgorithmTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace AIC.Functionality.Tests
+{
+    public class AlgorithmTests
+    {
+        [Fact(Skip = "Not implemented")]
+        public void CanFormatSeparators()
+        {
+            var list = new List<string> { "red", "green", "purple", "yellow", "pink" };
+
+            Assert.Equal("red, green, purple, yellow, and pink!", Algorithms.BuildFormattedString(list));
+        }
+
+
+        [Fact(Skip = "Not implemented")]
+        public void CanGetFactorial()
+        {
+            Assert.Equal(24, Algorithms.GetFactorial(4));
+        }
+    }
+}
